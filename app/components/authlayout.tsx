92a30a953dd102770df0f6052e06563ba9c56f1c
Akash kr <akashkr971@gmail.com>
2025-03-22 23:50:14 +0530

    Enable the authentication using supabase email authentication

---
+++ app/components/authlayout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Image from "next/image";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div 
+      className="flex items-center justify-center bg-cover bg-center bg-no-repeat" 
+      style={{ backgroundImage: "url('/bg-auth-1.jpg')" }}
+    >
+        <div className="w-full">
+            {children}  
+        </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
