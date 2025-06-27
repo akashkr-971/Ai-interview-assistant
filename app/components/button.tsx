623b9ef720050974f2ef495e62dbf262eec1e2eb
Akash kr <akashkr971@gmail.com>
2025-06-27 22:17:51 +0530

    Added table for payments and created page for interviewer

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -3,10 +3,9 @@ import React from "react";
 interface ButtonProps {
     text: string;
     onClick: () => void;
-    className?: string
 }
 
-const Button: React.FC<ButtonProps> = ({ text ,onClick ,className}) => {
+const Button: React.FC<ButtonProps> = ({ text ,onClick }) => {
     return (
         <button 
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded align-middle" type="submit"
